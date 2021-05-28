3b597a3e2717bfdead230ebee1e24049e2ffbe57
Ryan Dusmuir <ryan_dunsmuir@kansamples.com>
2021-05-28 17:43:48 +0100

    added average ratings

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,9 @@
 module ListsHelper
+  def list_background(list)
+    if list.photo.attached?
+      cl_image_path(@list.photo.key, height: 300, crop: :fill)
+    else
+      'https://images.unsplash.com/photo-1584213062606-7a1fc728727d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'
+    end
+  end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,12 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :photo
 
   validates :name, uniqueness: true, presence: true
+
+  def rating_average
+    total_rating = movies.sum { |movie| movie.rating }
+    (total_rating / movies.size).round(2)
+  end
 end
